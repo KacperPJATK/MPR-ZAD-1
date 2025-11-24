6ef53a2a033bcb86b3fbf42af40033f84941c159
Arkean_MAC <kacper_1000@onet.eu>
2025-11-24 01:55:29 +0100

    Analiza stażu pracy: Testy

---
+++ src/test/java/service/TenureServiceTest.java
@@ -0,0 +1,174 @@
+package service;
+
+import model.Employee;
+import model.Position;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import repository.EmployeesRepository;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+public class TenureServiceTest {
+
+    private TenureService tenureService;
+
+    private static void addTestEmployees() {
+        EmployeesRepository.add(new Employee(
+                "Leon", "Kennedy",
+                "kennedy@protonmail.com", "Capcom", Position.STAZYSTA,
+                LocalDate.parse("2024-10-12", DateTimeFormatter.BASIC_ISO_DATE)
+        ));
+        EmployeesRepository.add(new Employee(
+                "Chris", "Redfield",
+                "redfield@protonmail.com", "Capcom", Position.PROGRAMISTA,
+                LocalDate.parse("2015-9-20", DateTimeFormatter.BASIC_ISO_DATE)
+        ));
+        EmployeesRepository.add(new Employee(
+                "Nathan", "Drake",
+                "ndrake@protonmail.com", "NaughtyDog", Position.MANAGER,
+                LocalDate.parse("2020-5-1", DateTimeFormatter.BASIC_ISO_DATE)
+        ));
+        EmployeesRepository.add(new Employee(
+                "Sam", "Drake",
+                "sdrake@protonmail.com", "NaughtyDog", Position.WICEPREZES,
+                LocalDate.parse("2025-11-27", DateTimeFormatter.BASIC_ISO_DATE)
+        ));
+    }
+
+    public static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(1, 3),
+                Arguments.of(2, 2),
+                Arguments.of(5, 2),
+                Arguments.of(10, 1),
+                Arguments.of(15, 0)
+        );
+    }
+
+    @BeforeEach
+    void setup() {
+        tenureService = new TenureServiceImpl();
+        addTestEmployees();
+    }
+
+    @AfterEach
+    void clear() {
+        EmployeesRepository.clearForTest();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "kennedy@protonmail.com",
+            "redfield@protonmail.com",
+            "ndrake@protonmail.com",
+            "kennedy@protonmail.com"
+    })
+    void testCalculateTenure(String email) {
+//        given
+        Employee employee = EmployeesRepository.getEmployee(email);
+        long expected = Duration.between(employee.getEmploymentDate(), LocalDate.now()).toDays();
+//        when, then
+        MatcherAssert.assertThat(
+                tenureService.calculateTenure(email),
+                Matchers.is(Matchers.equalTo(expected))
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "kennedydoesntexist@protonmail.com",
+            "redfielddoesntexist@protonmail.com",
+            "ndrakedoesntexist@protonmail.com",
+            "kennedydoesntexist@protonmail.com"
+    })
+    void shouldFailWhenTryingToCalculateTenureForNonExistingEmployee() {
+//        when, then
+        Assertions.assertThatThrownBy(() -> tenureService.calculateTenure(email))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void shouldFailWhenTryingToCalculateTenureForNull() {
+//        when, then
+        Assertions.assertThatThrownBy(() -> tenureService.calculateTenure(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testGetAnniversaries() {
+//        when, then
+        MatcherAssert.assertThat(tenureService.getAnniversaries(), Matchers.allOf(
+                        Matchers.hasProperty(
+                                "email",
+                                Matchers.is(Matchers.equalTo("ndrake@protonmail.com"))
+                        ),
+                        Matchers.hasProperty(
+                                "email",
+                                Matchers.is(Matchers.equalTo("redfield@protonmail.com"))
+                        ),
+                        Matchers.hasSize(2)
+                )
+        );
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenThereAreNoEmployees() {
+//        given
+        EmployeesRepository.clearForTest();
+//        when, then
+        MatcherAssert.assertThat(tenureService.getAnniversaries(), Matchers.hasSize(0));
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void getEmployeesWithTenureLength(int tenureLength, int expectedSize) {
+//        when, then
+        MatcherAssert.assertThat(
+                tenureService.getEmployeesWithTenureLength(tenureLength),
+                Matchers.hasSize(expectedSize)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Capcom, 2",
+            "NaughtyDog, 2"
+    })
+    void testGetDemographicTeamReport(String companyName, int size) {
+//        when, then
+        MatcherAssert.assertThat(
+                tenureService.getDemographicReport(companyName),
+                Matchers.allOf(
+                        Matchers.hasProperty(
+                                "companyName", Matchers.is(Matchers.equalTo(companyName))
+                        ),
+                        Matchers.hasProperty("tenureMap", Matchers.hasSize(size))
+                )
+        );
+    }
+
+    @Test
+    void shouldReturnEmptyWhenThereAreNoEmployeesForTheReport() {
+//        given
+        EmployeesRepository.clearForTest();
+//        when, then
+        MatcherAssert.assertThat(
+                tenureService.getDemographicReport("someTeam"),
+                Matchers.hasSize(0)
+        );
+    }
+
+}
